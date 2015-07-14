186386c222e9d2206464b985d2d3688695452308
ejyoon <ejyoon@stanford.edu>
2015-07-13 22:56:32 -0700

    code with info for new audio file

--- experiment/spriteData.js
+++ experiment/spriteData.js
@@ -1,303 +1,303 @@
 var spriteData = {
 	apple_pos: {
-		start: 142.090,
-		onset: 142.090,
-		length: 1.2
+		start: 0,
+		onset: 0,
+		length: 0.297
 	},
-	apple_neg: {
-		start: 142.090,
-		onset: 142.090,
-		length: 1.2
+	apple_neg: { 
+		start: 16.787,
+		onset: 17.062,
+		length: 0.596
 	},
-	apple_inhib: {
-		start: 150.191,
-		onset: 150.191,
-		length: 1.2
+	apple_inhib: { // banana
+		start: 1.32,
+		onset: 1.32,
+		length: 0.379
 	},
 	apple_implic: {
-		start: 142.090,
-		onset: 142.090,
-		length: 1.2
+		start: 0,
+		onset: 0,
+		length: 0.297
 	},
 	apple_control: {
-		start: 142.090,
-		onset: 142.090,
-		length: 1.2
+		start: 0,
+		onset: 0,
+		length: 0.297
 	},
 	car_pos: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 5.547,
+		onset: 5.547,
+		length: 0.379
 	},
 	car_neg: {
-		start: 220.216,
-		onset: 220.216,
-		length: 1.00
+		start: 23.461,
+		onset: 23.723,
+		length: 0.587
 	},
-	car_inhib: {
-		start: 220.216,
-		onset: 220.216,
-		length: 1.00
+	car_inhib: { // bicycle
+		start: 2.726,
+		onset: 2.726,
+		length: 0.441
 	},
 	car_implic: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 5.547,
+		onset: 5.547,
+		length: 0.379
 	},
 	car_control: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 5.547,
+		onset: 5.547,
+		length: 0.379
 	},
 	dog_pos: {
-		start: 197.385,
-		onset: 197.385,
-		length: 1.00
+		start: 10.872,
+		onset: 10.872,
+		length: 0.422
 	},
 	dog_neg: {
-		start: 197.385,
-		onset: 197.385,
-		length: 1.00
+		start: 29.851,
+		onset: 29.851,
+		length: 0.689
 	},
-	dog_inhib: {
-		start: 189.434,
-		onset: 189.434,
-		length: 1.00
+	dog_inhib: { // cat
+		start: 6.952,
+		onset: 6.952,
+		length: 0.232
 	},
 	dog_implic: {
-		start: 197.385,
-		onset: 197.385,
-		length: 1.00
+		start: 10.872,
+		onset: 10.872,
+		length: 0.422
 	},
 	dog_control: {
-		start: 197.385,
-		onset: 197.385,
-		length: 1.00
+		start: 10.872,
+		onset: 10.872,
+		length: 0.422
 	},
 	cat_pos: {
-		start: 282.771,
-		onset: 282.771,
-		length: 1.00
+		start: 6.952,
+		onset: 6.952,
+		length: 0.232
 	},
 	cat_neg: {
-		start: 197.385,
-		onset: 197.385,
-		length: 1.00
+		start: 25.076,
+		onset: 25.366,
+		length: 0.541
 	},
-	cat_inhib: {
-		start: 197.385,
-		onset: 197.385,
-		length: 1.00
+	cat_inhib: { // bird
+		start: 4.191,
+		onset: 4.191,
+		length: 0.33
 	},
 	cat_implic: {
-		start: 282.771,
-		onset: 282.771,
-		length: 1.00
+		start: 6.952,
+		onset: 6.952,
+		length: 0.232
 	},
 	cat_control: {
-		start: 282.771,
-		onset: 282.771,
-		length: 1.00
+		start: 6.952,
+		onset: 6.952,
+		length: 0.232
 	},
 	banana_pos: {
-		start: 251.509,
-		onset: 251.509,
-		length: 1.00
+		start: 1.32,
+		onset: 1.32,
+		length: 0.379
 	},
 	banana_neg: {
-		start: 142.090,
-		onset: 142.090,
-		length: 1.2
+		start: 18.376,
+		onset: 18.631,
+		length: 0.674
 	},
-	banana_inhib: {
-		start: 142.090,
-		onset: 142.090,
-		length: 1.2
+	banana_inhib: { // cookie
+		start: 8.209,
+		onset: 8.209,
+		length: 0.289
 	},
 	banana_implic: {
-		start: 251.509,
-		onset: 251.509,
-		length: 1.00
+		start: 1.32,
+		onset: 1.32,
+		length: 0.379
 	},
 	banana_control: {
-		start: 251.509,
-		onset: 251.509,
-		length: 1.00
+		start: 1.32,
+		onset: 1.32,
+		length: 0.379
 	},
 	bicycle_pos: {
-		start: 126.429 ,
-		onset: 126.429 ,
-		length: 1.00
+		start: 2.726,
+		onset: 2.726,
+		length: 0.441
 	},
 	bicycle_neg: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 19.965,
+		onset: 20.226,
+		length: 0.759
 	},
-	bicycle_inhib: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+	bicycle_inhib: { // lamp
+		start: 13.851,
+		onset: 13.851,
+		length: 0.389
 	},
 	bicycle_implic: {
-		start: 126.429 ,
-		onset: 126.429 ,
-		length: 1.00
+		start: 2.726,
+		onset: 2.726,
+		length: 0.441
 	},
 	bicycle_control: {
-		start: 126.429 ,
-		onset: 126.429 ,
-		length: 1.00
+		start: 2.726,
+		onset: 2.726,
+		length: 0.441
 	},
 	lamp_pos: {
-		start: 393.149,
-		onset: 393.149,
-		length: 1.00
+		start: 13.851,
+		onset: 13.851,
+		length: 0.389
 	},
 	lamp_neg: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 33.204,
+		onset: 33.462,
+		length: 0.584
 	},
-	lamp_inhib: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+	lamp_inhib: { // car
+		start: 5.547,
+		onset: 5.547,
+		length: 0.379
 	},
 	lamp_implic: {
-		start: 393.149,
-		onset: 393.149,
-		length: 1.00
+		start: 13.851,
+		onset: 13.851,
+		length: 0.389
 	},
 	lamp_control: {
-		start: 393.149,
-		onset: 393.149,
-		length: 1.00
+		start: 13.851,
+		onset: 13.851,
+		length: 0.389
 	},
 	cup_pos: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 9.526,
+		onset: 9.526,
+		length: 0.157
 	},
 	cup_neg: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 28.235,
+		onset: 28.499,
+		length: 0.59
 	},
-	cup_inhib: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+	cup_inhib: { // flower
+		start: 12.378,
+		onset: 12.378,
+		length: 0.446
 	},
 	cup_implic: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 9.526,
+		onset: 9.526,
+		length: 0.157
 	},
 	cup_control: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 9.526,
+		onset: 9.526,
+		length: 0.157
 	},
 	teddybear_pos: {
-		start: 181.483,
-		onset: 181.483,
-		length: 1.00
+		start: 15.268,
+		onset: 15.268,
+		length: 0.493
 	},
 	teddybear_neg: {
-		start: 173.383,
-		onset: 173.383,
-		length: 1.00
+		start: 34.814,
+		onset: 35.124,
+		length: 0.755
 	},
-	teddybear_inhib: {
-		start: 173.383,
-		onset: 173.383,
-		length: 1.00
+	teddybear_inhib: { // cup
+		start: 9.526,
+		onset: 9.526,
+		length: 0.157
 	},
 	teddybear_implic: {
-		start: 181.483,
-		onset: 181.483,
-		length: 1.00
+		start: 15.268,
+		onset: 15.268,
+		length: 0.493
 	},
 	teddybear_control: {
-		start: 181.483,
-		onset: 181.483,
-		length: 1.00
+		start: 15.268,
+		onset: 15.268,
+		length: 0.493
 	},
 	flower_pos: {
-		start: 173.383,
-		onset: 173.383,
-		length: 1.00
+		start: 12.378,
+		onset: 12.378,
+		length: 0.446
 	},
 	flower_neg: {
-		start: 181.483,
-		onset: 181.483,
-		length: 1.00
+		start: 31.566,
+		onset: 31.865,
+		length: 0.614
 	},
-	flower_inhib: {
-		start: 181.483,
-		onset: 181.483,
-		length: 1.00
+	flower_inhib: { // teddybear
+		start: 15.268,
+		onset: 15.268,
+		length: 0.493
 	},
 	flower_implic: {
-		start: 173.383,
-		onset: 173.383,
-		length: 1.00
+		start: 12.378,
+		onset: 12.378,
+		length: 0.446
 	},
 	flower_control: {
-		start: 173.383,
-		onset: 173.383,
-		length: 1.00
+		start: 12.378,
+		onset: 12.378,
+		length: 0.446
 	},
 	cookie_pos: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 8.209,
+		onset: 8.209,
+		length: 0.289
 	},
 	cookie_neg: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 26.639,
+		onset: 26.909,
+		length: 0.569
 	},
-	cookie_inhib: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+	cookie_inhib: { // apple
+		start: 0,
+		onset: 0,
+		length: 0.297
 	},
 	cookie_implic: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 8.209,
+		onset: 8.209,
+		length: 0.289
 	},
 	cookie_control: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 8.209,
+		onset: 8.209,
+		length: 0.289
 	},
 	bird_pos: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 4.191,
+		onset: 4.191,
+		length: 0.33
 	},
 	bird_neg: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+		start: 21.753,
+		onset: 22.102,
+		length: 0.684
 	},
-	bird_inhib: {
-		start: 227.957,
-		onset: 227.957,
-		length: 1.00
+	bird_inhib: { // dog
+		start: 10.872,
+		onset: 10.872,
+		length: 0.422
 	},
 	bird_implic: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 4.191,
+		onset: 4.191,
+		length: 0.33
 	},
 	bird_control: {
-		start: 400.710,
-		onset: 400.710,
-		length: 1.00
+		start: 4.191,
+		onset: 4.191,
+		length: 0.33
 	},
 
 	banana: {
