f0a78b38528a8409b3b27bbedcd8140c7532f9ff
Ann <anordmey@stanford.edu>
2015-07-24 09:46:32 -0700

    added mturk data and analysis code updates

--- analysis/neginhib.R
+++ analysis/neginhib.R
@@ -7,11 +7,22 @@ library(dplyr)
 library(ggplot2)
 library(rjson)
 
-source("~/Documents/R/aen.useful.R")
+##functions
+# for bootstrapping 95% confidence intervals
+theta <- function(x,xdata) {mean(xdata[x])}
+ci.low <- function(x) {
+  quantile(bootstrap(1:length(x),1000,theta,x)$thetastar,.025)}
+ci.high <- function(x) {
+  quantile(bootstrap(1:length(x),1000,theta,x)$thetastar,.975)}
 
-##Load in data
-#turk demo data
-# path <- "~/Documents/Work/Research/Negation/neginhib/data/sandbox_pilot/sandbox-results/"
+
+## add some style elements for ggplot2
+plot.style <- theme_bw() + theme(panel.grid.minor=element_blank(), panel.grid.major=element_blank(), legend.position="right", axis.line = element_line(colour="black",size=.5), axis.ticks = element_line(size=.5), axis.title.x = element_text(vjust=-.5), axis.title.y = element_text(angle=90,vjust=0.25))
+
+
+# ##Load in data
+# turk demo data
+# path <- "~/Documents/Work/Research/Negation/neginhib/data/adults_mturk/production-results/"
 # files <- dir(path)
 # 
 # demo <- as.data.frame(matrix(ncol = 4, nrow = 0))
@@ -26,21 +37,27 @@ source("~/Documents/R/aen.useful.R")
 #   tmp <- cbind(subid, age, gender, language)
 #   demo <- rbind(demo, tmp)  
 # }
+# 
+# #csv data
+# path <- "~/Documents/Work/Research/Negation/neginhib/data/adults_mturk/"
+# files <- dir(path)
+# 
+# d <- data.frame()
+# 
+# for (f in files) {
+#   this.file <- read.csv(paste(path, f, sep=""), header=FALSE)
+#   names(this.file) <- c("subid","list","game.order","trial.num","word","leftpic","rightpic","game","trial.type","leftpic.type","rightpic.type","side.chosen","pic.chosen","response","date","timestamp","rt")
+#   d <- rbind(d, this.file)  
+# }
+# 
+# #merge data
+# d <- merge(d, demo)
+# 
+# #anonymize subid
+# d$subid <- as.factor(as.numeric(as.factor(d$subid)))
 
-#csv data
-path <- "~/Documents/Work/Research/Negation/neginhib/data/sandbox_pilot/"
-files <- dir(path)
 
-d <- data.frame()
-
-for (f in files) {
-  this.file <- read.csv(paste(path, f, sep=""), header=FALSE)
-  names(this.file) <- c("subid","list","game.order","trial.num","word","leftpic","rightpic","game","trial.type","leftpic.type","rightpic.type","side.chosen","pic.chosen","response","date","timestamp","rt")
-  d <- rbind(d, this.file)  
-}
-
-#merge data
-#d <- merge(expt, demo)
+d <- read.csv("~/Documents/Work/Research/Negation/neginhib/long_data/long_data_mturk.csv")
 
 ##Prep data
 d$correct <- as.numeric(as.character(factor(d$response, levels=c("Y","N"), labels=c("1","0"))))
@@ -69,6 +86,10 @@ dc <- filter(d, correct==1)
 
 #log transform
 #LogRT
+
+#remove negative RTs...(why/how did this happen??)
+dc <- dc[dc$rt >= 0,]
+
 qplot(data=dc, x=rt, geom="histogram")
 dc$log.rt<-log(dc$rt)
 qplot(data=dc, x=log.rt, geom="histogram")
